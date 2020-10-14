2252ccddbe1438418c279483878f8659165de8ac
VNN-oss <70770522+VNN-oss@users.noreply.github.com>
2020-10-14 10:24:31 -0500

    Create player.java

---
+++ src/player.java
@@ -0,0 +1,13 @@
+package player;
+
+public class Player {
+    public Player(int retCords) {
+        this.retCords = retCords;
+        int x;
+        int y;
+    }
+    private float retCords{
+         ret =[Player.x,Player.y]
+        return ret;
+    }
+}
